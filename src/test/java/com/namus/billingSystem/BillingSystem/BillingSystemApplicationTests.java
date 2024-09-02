74bf05d9c7f5719de31e1f0c3e6f3fb477aea2eb
Arun Neupane <88606482+arunneupane332@users.noreply.github.com>
2024-09-02 22:21:37 +0545

    Task Started

---
+++ src/test/java/com/namus/billingSystem/BillingSystem/BillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namus.billingSystem.BillingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
